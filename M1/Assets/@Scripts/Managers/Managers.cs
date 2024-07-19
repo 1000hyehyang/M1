fbeea10693efb08d82401d3cf6584cb59349ba45
1000hyehyang <102294782+1000hyehyang@users.noreply.github.com>
2024-07-20 02:20:08 +0900

    settings : folder

--- M1/Assets/@Scripts/Managers/Managers.cs
+++ M1/Assets/@Scripts/Managers/Managers.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 // singleton으로 모든 기능 관리
 public class Managers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /* 유니티에서는 오브젝트를 판 다음에 컴포넌트라는 부품을 붙여줘야 적용이 됨. 비로소 코드가 활성화되는 것임.
+     * 그 작업을 일일이 hierachy 창에서 하는 게 아니라 아래 코드로 생성해주는 것임 */
 
-    // Update is called once per frame
-    void Update()
+    private static Managers s_instance;
+    private static Managers Instance {  get { Init();  return s_instance; } }
+
+
+    public static void Init() 
     {
-        
+        if (s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go != null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+
+            // 초기화
+            s_instance = go.GetComponent<Managers>();
+        }
     }
 }
